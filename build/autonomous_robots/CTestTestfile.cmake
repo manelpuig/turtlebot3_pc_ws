fc071acf4192d313ceecd36cd723c1f85a0f3dec
Manel Puig <manel.puig@ub.edu>
2020-12-31 14:16:57 +0100

    Thursday6

---
+++ build/autonomous_robots/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mpuig/turtlebot3_pc_ws/src/autonomous_robots
+# Build directory: /home/mpuig/turtlebot3_pc_ws/build/autonomous_robots
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
